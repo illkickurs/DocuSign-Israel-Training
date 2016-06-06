e7c3f995c144041659476d06e494ff48eaaa7ab6
OferLevi <ofer.levi@docusign.com>
2016-06-06 17:14:29 +0300

    Add Hello World Project
    This project is the first one to be added to the repository. Note that it is committed to the Hello-World branch (and not the master branch).
    Later on, when I will finish working on this project, I will push the changes from this branch to the master branch.

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,13 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
